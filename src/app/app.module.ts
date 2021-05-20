6bdefc62cdaf8ac6fe87d6ce9543548a1979bb2d
Rafal Nowak <rafalnowak62@o2.pl>
2021-05-20 17:01:13 +0200

    Added sidebar and menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
